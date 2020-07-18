2b3df8635d9c50d253be60998e30ae3bc525a8bb
Jan Klaas Kollhof <jan@kollhof.net>
2020-07-18 20:12:49 +0200

    feat: upgrade to latest lang

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
   watchPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
 
-  timers: 'fake',
+  timers: 'modern',
 
   clearMocks: true,
   resetMocks: false,
